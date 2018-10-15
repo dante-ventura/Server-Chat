d32dd59c483f347e538dea9123be43926eca8083
OTHSCS073 <OTHSCS073@KATYISD.ORG>
2018-10-15 08:13:37 -0500

    message boxes formatted

--- public/client.js
+++ public/client.js
@@ -1,9 +1,32 @@
-
+var messageCount = 1;
 
 function sendMessage(e){
     if(e.keyCode === 13){
         e.preventDefault();
+
+        document.getElementById(`m${messageCount}`).insertAdjacentHTML('afterend', 
+       `<div class="box" id="m${++messageCount}">
+        <article class="media">
+          <div class="media-left">
+            <figure class="image is-64x64">
+              <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image">
+            </figure>
+          </div>
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+                <br>
+                ${e.currentTarget.value}
+              </p>
+            </div>
+          </div>
+        </article>
+      </div>`)
+
         e.currentTarget.value = '';
-        alert("Your message was sent.");
+
+
+        //alert("Your message was sent.");
     }
 }
